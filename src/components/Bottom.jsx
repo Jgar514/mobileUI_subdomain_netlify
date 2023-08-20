ee53ce9239494e65c9626c0c0592c6cb0443848d
Joshua Garvey <j.garvey514@yahoo.com>
2023-08-20 19:23:51 -0400

    updated bottom social links

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -9,16 +9,19 @@ function Bottom() {
 		<div className="bg-izzy-500 h-1/6 flex flex-row border-black border-t-2 border-b-2 flex-wrap items-center justify-center p-2  ">
 			<div className="bg-manilla-100 border-2 border-black w-full rounded-3xl items-center  overflow-hidden    flex h-full px-0 z-50">
 				<div className="text-6xl w-full flex gap-8 py-0 px-4 ">
-					<a href="https://github.com/Jgar514">
+					<button role="link" onClick={() => openInNewTab("https://www.linkedin.com/in/josh-garvey-05944825a/")}>
 						<AiFillGithub />
-					</a>
+					</button>
+					{/* <a href="https://github.com/Jgar514">
+						<AiFillGithub />
+					</a> */}
 					<button role="link" onClick={() => openInNewTab("https://www.linkedin.com/in/josh-garvey-05944825a/")}>
 						<AiFillLinkedin />
 					</button>
-
-					<a href="https://www.instagram.com/joshgarvey/">
+					<button role="link" onClick={() => openInNewTab("https://www.instagram.com/joshgarvey/")}>
 						<AiOutlineInstagram />
-					</a>
+					</button>
+
 					<a href="">
 						<AiOutlineSend />
 					</a>
