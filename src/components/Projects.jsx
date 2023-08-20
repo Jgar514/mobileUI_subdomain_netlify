62e0518383b65f08fe2b129b1b2708d809134c5d
Joshua Garvey <j.garvey514@yahoo.com>
2023-08-20 15:30:48 -0400

    added another project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,8 +33,10 @@ function Projects() {
 			</div>
 
 			<div className="keen-slider__slide number-slide3 flex justify-center items-center">
-				<div className=" bg-[url('./images/dash.png')] h-6/6 bg-cover rounded-xl border-2 border-black flex aspect-square  object-scale-down bg-center">
-					<h1 className="">Immersive 3d portfolio</h1>
+				<div className=" bg-[url('./images/squaremendix.png')] h-6/6 bg-cover rounded-xl border-2 border-black flex aspect-square  object-scale-down bg-center items-end">
+					<div className="w-full h-1/6 flex  items-center justify-center bottom-0 backdrop-brightness-50">
+						<span className="text-white text-lg text-bold   ">Enterprise Dashboard</span>
+					</div>
 				</div>
 			</div>
 
